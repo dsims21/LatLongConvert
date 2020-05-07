7e187cdd043731e2aa992c2ca04191896bcde979
Daniel Sims <dsims@spearhead-capm.com>
2020-05-07 16:46:35 -0700

    Works, but will take a long time.

--- LatLongConvert.py
+++ LatLongConvert.py
@@ -18,7 +18,7 @@ def get_county(lat, long):
     address = location.raw['address']
     county = address.get('county', '')
 
-    return county
+    return str(county)
 
 def get_ZIP(lat, long):
     coord = str(lat)+', '+str(long)
@@ -26,7 +26,7 @@ def get_ZIP(lat, long):
     address = location.raw['address']
     ZIP = address.get('postcode', '')
 
-    return ZIP
+    return str(ZIP)
 
 def get_country(lat, long):
     coord = str(lat)+', '+str(long)
@@ -34,7 +34,7 @@ def get_country(lat, long):
     address = location.raw['address']
     country = address.get('country', '')
 
-    return country
+    return str(country)
 
 geolocator = Nominatim(user_agent="MyGeocoder")
 
@@ -47,17 +47,25 @@ print(city_state_country("47.6144239, -122.19215913827682"))
 
 
 fields = ['ID', 'Latitude', 'Longitude']
-df = pd.read_csv("ghcnd-stations-lat-long-short.csv",)
+df = pd.read_csv("ghcnd-stations-lat-long.csv",)
 
+#df["Country"] = df.apply(lambda x: city_state_country("47.6144239, -122.19215913827682"), axis=1)
 df["Country"] = ""
 df["County"] = ""
 df["ZIP"] = ""
 
 for index, row in df.iterrows():
     #print(row['c1'], row['c2'])
-    row['Country'] = get_country(row['Latitude'],row['Longitude'])
-    row['County'] = get_county(row['Latitude'], row['Longitude'])
-    row['ZIP'] = get_ZIP(row['Latitude'], row['Longitude'])
+    #country = get_country(row['Latitude'],row['Longitude'])
+
+    df.loc[df.index[index], 'Country'] = get_country(row['Latitude'],row['Longitude'])
+    df.loc[df.index[index], 'County'] = get_county(row['Latitude'], row['Longitude'])
+    df.loc[df.index[index], 'ZIP'] = get_ZIP(row['Latitude'], row['Longitude'])
+
+    print('Row:'+ str(index))
+    #row['Country'] = get_country(row['Latitude'],row['Longitude'])
+    #row['County'] = str(get_county(row['Latitude'], row['Longitude']))
+    #row['ZIP'] = get_ZIP(row['Latitude'], row['Longitude'])
 
 df.to_csv("sample.csv", index=False)
 
