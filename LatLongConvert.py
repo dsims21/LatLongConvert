bf22c17630e0f8c9bbecaa60d5f99fb698f200bb
Daniel Sims <dsims@spearhead-capm.com>
2020-05-12 10:39:47 -0700

    Added minor error handling.

--- LatLongConvert.py
+++ LatLongConvert.py
@@ -1,5 +1,6 @@
 from geopy.geocoders import Nominatim
 import pandas as pd
+import time
 
 def country_state_county_zip(lat, long):
     coord = str(lat) + ', ' + str(long)
@@ -11,6 +12,8 @@ def country_state_county_zip(lat, long):
     ZIP = address.get('postcode', '')
 
     return country, state, county, ZIP
+
+counter = 0
 
 geolocator = Nominatim(user_agent="MyGeocoder")
 
@@ -30,18 +33,26 @@ for index, row in df.iterrows():
     if 49.196064 > lat > 45.576501 and -125.375188 < long < -116.821468:
     #Entire US
     #if 22.824922 < lat < 49.774613  and -126.196032 < long < -64.103819:
-        #Get data
-        data = country_state_county_zip(row['Latitude'],row['Longitude'])
+        try:
 
-        #Add data pieces to columns in the dataframe
-        df.loc[df.index[index], 'Country'] = data[0]
-        df.loc[df.index[index], 'State'] = data[1]
-        df.loc[df.index[index], 'County'] = data[2]
-        df.loc[df.index[index], 'ZIP'] = data[3]
+            #Get data
+            data = country_state_county_zip(lat,long)
 
-        #Just for debugging/interest, print line found.
-        print('Row: '+ str(index))
+            #Add data pieces to columns in the dataframe
+            df.loc[df.index[index], 'Country'] = data[0]
+            df.loc[df.index[index], 'State'] = data[1]
+            df.loc[df.index[index], 'County'] = data[2]
+            df.loc[df.index[index], 'ZIP'] = data[3]
+
+            #Just for debugging/interest, print line found.
+            print('Row: '+ str(index))
+            counter +=1
+
+        except:
+            print('Error on row: ' + str(index) + ' Counter: ' + str(counter))
+            time.sleep(5)
+
 
 df.to_csv("sample.csv", index=False)
-
+print('Completed ' + str(counter) + ' rows.')
 print('Done')
