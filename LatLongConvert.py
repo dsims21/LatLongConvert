ec067b2446ba5fe9fcb1ee9000289ad6325947ab
Daniel Sims <dsims@spearhead-capm.com>
2020-05-13 13:27:57 -0700

    Added looping file search to recover from errors.

--- LatLongConvert.py
+++ LatLongConvert.py
@@ -13,46 +13,51 @@ def country_state_county_zip(lat, long):
 
     return country, state, county, ZIP
 
-counter = 0
-
 geolocator = Nominatim(user_agent="MyGeocoder")
 
-df = pd.read_csv("ghcnd-stations-lat-long.csv",)
+#df = pd.read_csv("ghcnd-stations-lat-long.csv",)
+df = pd.read_csv("sample.csv",)
 
-df["Country"] = ""
-df["State"] = ""
-df["County"] = ""
-df["ZIP"] = ""
+#Create columns if they dont exist (ie: if the file has not been run before)
+if 'Country' not in df:
+    df["Country"] = ""
+if 'State' not in df:
+    df["State"] = ""
+if 'County' not in df:
+    df["County"] = ""
+if 'ZIP' not in df:
+    df["ZIP"] = ""
 
-for index, row in df.iterrows():
+for i in range(3): #Run through the below loop three times to make sure none missed due to errors.
+    for index, row in df.iterrows():
 
-    lat = float(row['Latitude'])
-    long = float(row['Longitude'])
+        lat = float(row['Latitude'])
+        long = float(row['Longitude'])
 
-    #Initially checking just WA state
-    if 49.196064 > lat > 45.576501 and -125.375188 < long < -116.821468:
-    #Entire US
-    #if 22.824922 < lat < 49.774613  and -126.196032 < long < -64.103819:
-        try:
+        #Initially checking just WA state
+        #if 49.196064 > lat > 45.576501 and -125.375188 < long < -116.821468:
+        #Entire US
+        if 22.824922 < lat < 49.774613  and -126.196032 < long < -64.103819 \
+                and pd.isnull(df.loc[df.index[index], 'Country']) == True:
+            try:
 
-            #Get data
-            data = country_state_county_zip(lat,long)
+                #Get data
+                data = country_state_county_zip(lat,long)
 
-            #Add data pieces to columns in the dataframe
-            df.loc[df.index[index], 'Country'] = data[0]
-            df.loc[df.index[index], 'State'] = data[1]
-            df.loc[df.index[index], 'County'] = data[2]
-            df.loc[df.index[index], 'ZIP'] = data[3]
+                #Add data pieces to columns in the dataframe
+                df.loc[df.index[index], 'Country'] = data[0]
+                df.loc[df.index[index], 'State'] = data[1]
+                df.loc[df.index[index], 'County'] = data[2]
+                df.loc[df.index[index], 'ZIP'] = data[3]
 
-            #Just for debugging/interest, print line found.
-            print('Row: '+ str(index))
-            counter +=1
+                #Just for debugging/interest, print line found.
+                print('Row: '+ str(index))
 
-        except:
-            print('Error on row: ' + str(index) + ' Counter: ' + str(counter))
-            time.sleep(5)
+            except:
+                print('Error on row: ' + str(index))
+                time.sleep(5)
+                df.to_csv("finalError.csv", index=False)
 
-
-df.to_csv("sample.csv", index=False)
-print('Completed ' + str(counter) + ' rows.')
-print('Done')
+#Export to new CSV
+df.to_csv("final.csv", index=False)
+print('Completed')
